4a37494ab6b18e41f2e792e22db2b5a835b70e0f
IvanRyabenkov <114497282+IvanRyabenkov@users.noreply.github.com>
2022-12-19 18:42:12 +0300

    new settings

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import numpy as np
 WIDTH = 1000
 HEIGHT = 600
 FPS = 120
-PIX = 50
+PIX = 25
 FPS_POS = (WIDTH - 50, 10)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -17,12 +17,15 @@ BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, GREY]
 
-player_pos = (WIDTH // 2 , HEIGHT // 2)
-player_angle = 0
+nn =(WIDTH // PIX) // 2 - 4
+mm = (HEIGHT // PIX) // 2 - 4
+
+player_pos = ((WIDTH // nn), (HEIGHT // mm))
+player_angle = math.pi / 2
 player_speed = 2
 
 FOV = math.pi / 3 #field of views
-NUM_RAYS = 300
+NUM_RAYS = 500
 MAX_DEPTH = 800
 DELTA_ANGLE = FOV / NUM_RAYS
 SCALE = WIDTH // NUM_RAYS
@@ -30,4 +33,7 @@ DIST = NUM_RAYS / (2 * math.tan(FOV/2))
 PROJECTION_k = 3 * DIST * PIX
 
 MINIMAP_SCALE = 5
-MINIMAP_PIX = PIX // MINIMAP_SCALE
+MINIMAP_PIX =PIX // MINIMAP_SCALE
+
+TIME_POS = (WIDTH - 150, 20)
+TIMER_POS = (WIDTH - 100, 20)
